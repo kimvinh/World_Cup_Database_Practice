575a0cd793e387121349863844d8180ec0e4df5c
Vinh Tran <42789132+kimvinh@users.noreply.github.com>
2023-04-18 23:54:15 -0700

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -16,18 +16,23 @@ do
   # Insert the data into 'teams' table
   if [[ $YEAR != year ]]
   then
-    echo Inserting winner
-    echo "$($PSQL "INSERT INTO teams(name) VALUES('$WINNER') ON CONFLICT DO NOTHING")"
+    WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")"
+    if [[ -z $WINNER_ID ]]
+    then
+      echo "$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")"
+    fi
 
-    echo Inserting opponent
-    echo "$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') ON CONFLICT DO NOTHING")"
+    OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")"
+    if [[ -z $OPPONENT_ID ]]
+    then
+      echo "$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")"
+    fi
 
     # Get new 'winner_id' and 'oppoent_id'
     WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")"
     OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")"
 
     # Insert the data into 'games' table
-    echo Inserting games
     echo "$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")"
   fi
 done
